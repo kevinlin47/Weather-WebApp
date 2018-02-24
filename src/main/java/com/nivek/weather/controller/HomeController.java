809535e96c050731cf546f57765425fdd3df42e0
Kevin Lin <kevinlin47@hotmail.com>
2018-02-24 00:29:52 -0500

    Trying to add city search feature

--- src/main/java/com/nivek/weather/controller/HomeController.java
+++ src/main/java/com/nivek/weather/controller/HomeController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,5 +16,20 @@ public class HomeController {
 	@RequestMapping(value="/weather",method=RequestMethod.GET)
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/weather",method=RequestMethod.POST)
+	public ModelAndView getCitySearch(HttpServletResponse response,@RequestParam(value="city")String city) throws IOException
+	{	
+		return new ModelAndView("CityWeather");
+	}
+	
+	@RequestMapping(value="/CityWeather", method=RequestMethod.GET)
+	public ModelAndView displayCityWeather(HttpServletResponse response) throws IOException
+	{
+		return new ModelAndView("CityWeather");
+	}
+	{
+		
 	}
 }
