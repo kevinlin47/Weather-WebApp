bf09bc4d185478276b727efc35f05ddbcd2a5914
Kevin Lin <kevinlin47@hotmail.com>
2018-03-06 11:26:18 -0500

    Added home page mapping

--- src/main/java/com/nivek/weather/controller/HomeController.java
+++ src/main/java/com/nivek/weather/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-
+	@RequestMapping("/")
+	public String home(ModelMap map)
+	{
+		return "home";
+	}
+	
 	@RequestMapping(value="/weather",method=RequestMethod.GET)
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
