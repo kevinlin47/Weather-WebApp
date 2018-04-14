75575c4a23128fbd5a6d1c3d24099d3e51366eb3
Kevin Lin <kevinlin47@hotmail.com>
2018-04-13 23:44:37 -0400

    LocalTime Class

---
+++ src/main/java/com/nivek/weather/config/LocalTime.java
@@ -0,0 +1,15 @@
+package com.nivek.weather.config;
+
+public class LocalTime {
+	
+	private String error;
+	private String error_message;
+	private String time;
+	private String timeZone;
+	private String offSet;
+	private String daylight_savings;
+	
+	
+	
+
+}
